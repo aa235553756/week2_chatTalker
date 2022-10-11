1ffca57bdde41258fcbfc86c2fab6e7d0bc0b137
ChiaYang <aa235553756@gmail.com>
2022-10-11 17:27:04 +0800

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,41 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+          // '2xl': { max: '1535px' },
+          // xl: { max: '1279px' },
+          // lg: { max: '1023px' },
+          // md: { max: '767px' },
+          // sm: { max: '639px' },
+  
+        minsm: { min: '640px' },
+        minmd: { min: '768px' },
+        minlg: { min: '1024px' },
+        minxl: { min: '1280px' },
+        min2xl: { min: '1536px' },
+      },
       fontFamily:{
         'sans': ['Noto Sans TC', 'sans-serif']
       },
       backgroundImage: {
-        'home-sm': "url('/images/bg_home_sm.svg')",
       },
-      screens:{
-        // 1088+20x2 px
-        'xl':'1128px',
-      },
+      // screens:{
+      //   // 1088+20x2 px
+      //   // 1128
+      //   'xl':{min:'1128px'},
+      //   // 768+20x2
+      //   // 992時更動
+      //   'lg':{min:'808px'},
+      //   // 696+20x2
+      //   // 768時更動
+      //   'md':{min:'736px'},
+      // },
+      // screens: {
+      //   sm: '480px',
+      //   md: '768px',
+      //   lg: '976px',
+      //   xl: '1440px',
+      // },
       container: {
         center: true,
         padding: "20px"
